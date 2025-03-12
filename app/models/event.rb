6c215ee32101a639fad0ef8a497c86797605fc36
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-03-13 01:02:57 +0530

    Model: Add "Active Record" associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
+  has_many :invitations
+  has_many :attendees, through: :invitations
 
   validates :creator_id, presence: true
   validates :title, presence: true, uniqueness: true
