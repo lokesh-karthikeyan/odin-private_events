32f7676324652e16250f83b266e5c271184b1031
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-03-13 01:04:33 +0530

    DB: Update "Schema migration"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_12_170240) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_12_190631) do
   create_table "events", force: :cascade do |t|
     t.date "date"
     t.datetime "created_at", null: false
@@ -19,6 +19,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_12_170240) do
     t.string "title"
     t.text "description"
     t.integer "creator_id"
+  end
+
+  create_table "invitations", force: :cascade do |t|
+    t.integer "event_id"
+    t.integer "attendee_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
