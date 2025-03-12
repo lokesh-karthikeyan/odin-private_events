b6b310120a38af1f803e55dc75bde96f537a1ceb
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-03-12 23:39:41 +0530

    Model: Add "validations" to persist data

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
+
+  validates :creator_id, presence: true
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :date, presence: true
+  validates :location, presence: true
 end
