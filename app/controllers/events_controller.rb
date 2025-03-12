1ad05be4edcae31d12e14676efb846f680074553
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-03-12 21:18:16 +0530

    Controller: Add `#new, #create` action controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,20 @@
 class EventsController < ApplicationController
   def index = (@event = Event.all)
+
+  def new = (@event = Event.new)
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    if @event.save
+      redirect_to event_path, success: "An event was successfully created!"
+    else
+      render :new, status: :unprocessable_entity
+      flash.now[:error] = "An error occured!"
+    end
+  end
+
+  private
+
+  def event_params = (params.expect(event: [ :title, :description, :date, :location ]))
 end
