dce95b3391598d9009e5fbd5dc0a2d8194693206
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-03-13 12:23:31 +0530

    Controller: Add `#create, #destroy` action controller

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,2 +1,15 @@
 class InvitationsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @event = Event.find(params[:event_id])
+    @event.attendees << current_user
+    redirect_to @event, notice: "You've been admitted to this event."
+  end
+
+  def destroy
+    @event = Event.find(params[:event_id])
+    @event.attendees.delete(current_user)
+    redirect_to @event, notice: "Event is removed from your schedule."
+  end
 end
