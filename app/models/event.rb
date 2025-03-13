f034a75bccba02ccf9305e03b2993ab5bece0607
lokesh-karthikeyan <172601273+lokesh-karthikeyan@users.noreply.github.com>
2025-03-13 13:17:55 +0530

    Model: Add "Scope" to filter data from db

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,11 @@
 class Event < ApplicationRecord
+  scope :past, -> { where("date < ?", Time.now) }
+  scope :upcoming, -> { where("date > ?", Time.now) }
+
+
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
   has_many :invitations
-  has_many :attendees, through: :invitations
+  has_many :attendees, through: :invitations, source: :attendee
 
   validates :creator_id, presence: true
   validates :title, presence: true, uniqueness: true
